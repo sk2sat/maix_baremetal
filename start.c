426cb5ad1cbf9d39fe55e4a29c144c8b7c35306c
sksat <sksat@sksat.net>
2020-03-22 23:06:01 +0900

    [add] use multi-core

--- start.c
+++ start.c
@@ -1,6 +1,25 @@
-void start(int core){
-	char *p = (char*)0x38000000;
+#define UART_ADDR	0x38000000
+
+void core0_main();
+void core1_main();
+
+void start(int core_number){
+	if(core_number == 0)
+		core0_main();
+	else
+		core1_main();
+}
+
+void core0_main(){
+	volatile char *p = (char*)UART_ADDR;
 	while(1){
-		*p = 'A';
+		*p = '0';
+	}
+}
+
+void core1_main(){
+	volatile char *p = (char*)UART_ADDR;
+	while(1){
+		*p = '1';
 	}
 }
